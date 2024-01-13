db636365f79b7efabac7859b6e41b1a47391fe13
Nik Gospodinov <nik.gospodinov@bt.com>
2024-01-13 11:54:17 +0000

    register users

--- actions/register.action.ts
+++ actions/register.action.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "../schemas";
+import { getUserByEmail } from "@/utils/db/user.utils";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { status: "error", message: "There was a problem registering" };
   }
+
+  const { email, password, name } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { status: "error", message: "Email already in use" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send email confirmation
 
   return { status: "success", message: "User registered" };
 };
